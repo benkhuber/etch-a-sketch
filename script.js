fbf3374138f5e3f421eeec7403786f818e8ba515
Ben Huber <benkhuber@gmail.com>
2021-10-26 21:43:51 -0700

    Updated index.html, script.js, style.css

--- script.js
+++ script.js
@@ -5,18 +5,15 @@ function makeGrid(rows, columns) {
     container.style.setProperty('--grid-columns', columns);
     for (i = 0; i < (rows * columns); i++) {
         const div = document.createElement('div');
-        container.appendChild(div).className = "gridcell";
+        div.classList.add('gridcell');
+        container.appendChild(div)
     }
 }
 
 makeGrid(16,16);
 
-const newDiv = document.querySelector('div');
-
-function changeBackground() {
-    newDiv.classList.toggle('hover');
-    console.log("hover!");
-    true
-}
-
-newDiv.addEventListener('mouseover', changeBackground);
+const newDiv = document.querySelector('.gridcell');
+window.addEventListener('mouseover', function(e){
+    newDiv.classList.add('hover');
+    console.log(e);
+})
